adcaad1a031fd380476e85167a5ab1a25464b6dd
skanda <skandakk@gmail.com>
2018-04-04 12:17:11 -0400

    model works...it seems like

--- keras_tut.py
+++ keras_tut.py
@@ -1,4 +1,7 @@
 # tutorial from elitedatascience.com -- including comments for my benefit
+# https://elitedatascience.com/keras-tutorial-deep-learning-in-python
+# "augmented" with my own documentation
+
 
 import numpy as np
 from keras.models import Sequential
@@ -22,12 +25,69 @@ def main():
     # pixels
 
     # showing the plot (ie, the first 28 x 28 image)
-    print("reaching here!")
-    plt.show(X_train[0])
-    print("not reaching here")
-    plt.show()
-    print("yo what the fuck??")
-    plt.savefig("mypic.png")
+    # for some stupid fucking reason matplotlib doesn't work
+    # matplotlib not working atm
+    # print("reaching here!")
+    # plt.show(X_train[0])
+    # print("not reaching here")
+    # plt.show()
+    # print("yo what the fuck??")
+    # plt.savefig("mypic.png")
+
+    X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+    X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+
+    # printing X's dimensions once again
+    # Split data into testing and training data
+    print(X_train.shape)
+
+    # converting datatype to float32 and normalizing data values to range [0, 1]
+    X_train = X_train.astype("float32")
+    X_test = X_test.astype("float32")
+
+    # normalizes 32 bit float
+    X_train /= 255
+    X_test /= 255
+
+    print("ytrainshape:", y_train.shape)
+
+    print(y_train[:10])
+
+    # what we saw above isn't what we want. we want something that 
+    # not entirely sure what these lines here do
+    Y_train = np_utils.to_categorical(y_train, 10)
+    Y_test = np_utils.to_categorical(y_test, 10)
+
+    print(Y_train.shape)
+
+
+    # building neural network
+    model = Sequential()
+    model.add(Convolution2D(32, 3, 3, activation="relu", input_shape=(28, 28, 1)))
+    # adding more layers to model like we're building legos xD
+    model.add(Convolution2D(32, 3, 3, activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    # Dropout layer to prevent overfitting
+    model.add(Dropout(0.25))
+
+    # adding a fully connected layer and output layer
+    model.add(Flatten())
+    model.add(Dense(128, activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation="softmax"))
+
+    model.compile(loss="categorical_crossentropy",
+                  optimizer="adam",
+                  metrics=["accuracy"])
+    
+    model.fit(X_train, Y_train, batch_size=32, epochs=10, verbose=1)
+
+    score = model.evaluate(X_test, Y_test, verbose = 0)
+
+    print(model.output_shape)
+    
+
+
 
 if __name__ == "__main__":
     main()
